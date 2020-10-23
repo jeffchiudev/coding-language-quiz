8af61bfea64601e3ced0d252e960e5e3044fdcea
Jeff Chiu <jwchiu82@gmail.com>
2020-10-23 11:14:41 -0700

    fix NaN bug in js to properly sum values from quiz

--- js/scripts.js
+++ js/scripts.js
@@ -4,13 +4,13 @@ $(document).ready(function() {
     event.preventDefault();
   });
   $("#quiz-form").submit(function(event) {
-    let total = 0;
     const musicValue = parseInt($("input:radio[name=music]:checked").val());
     const foodValue = parseInt($("input:radio[name=food-host]:checked").val());
-    const animalValue = parseInt($("intput:radio[name=animal]:checked").val());
+    const animalValue = parseInt($("input:radio[name=animal]:checked").val());
     const placeValue = parseInt($("input:radio[name=place]:checked").val());
     const colorValue = parseInt($("input:radio[name=color]:checked").val());
-    console.log(total + colorValue);
+    let total = colorValue + placeValue + animalValue + foodValue + musicValue;
+    console.log(total);
     event.preventDefault();
   });
 });
